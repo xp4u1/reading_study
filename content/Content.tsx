d14ffd4ded580af2c7ccc1eb5a2067e564574b14
Paul Hoerenz <development@hoerenz.com>
2023-09-22 22:51:07 +0200

    💬 Shorten the "lehrerin" text and remove questions

--- content/Content.tsx
+++ content/Content.tsx
@@ -22,6 +22,68 @@ export interface Text {
 
 export const content: Text[] = [
   {
+    id: "lehrerin",
+    component: <Lehrerin />,
+    bionicComponent: <LehrerinBionic />,
+    questions: [
+      {
+        // Lösung: B
+        id: "gefuehle_beruf",
+        text: "Was empfindet die Autorin, wenn sie ihren Beruf nennt?",
+        answers: [
+          "Sie fühlt sich besonders wichtig.",
+          "Sie hat gemischte Gefühle.",
+          "Sie ist wütend auf die Fragenden.",
+          "Sie empfindet nichts.",
+        ],
+      },
+      {
+        // Lösung: A
+        id: "warum_lehrer",
+        text: "Warum ergriff die Autorin den Lehrerberuf?",
+        answers: [
+          "Sie wollte jungen Menschen etwas fürs Leben mitgeben.",
+          "Sie suchte eine Arbeit, die viel Freiraum bietet.",
+          "Ihr gefiel die Sicherheit des Arbeitsplatzes.",
+          "Sie suchte eine Stelle mit Verantwortung.",
+        ],
+      },
+      {
+        // Lösung: C
+        id: "oeffentlichkeit_meinung",
+        text: "Wie steht nach Meinung der Autorin die Öffentlichkeit zum Beruf des Lehrers?",
+        answers: [
+          "Man gibt den Lehrern Schuld an gesellschaftlichen Missständen.",
+          "Die vormals negative Beurteilung der Lehrer ist zurückgegangen.",
+          "Die Beurteilung schwankt zwischen Anerkennung und harter Kritik.",
+          "Man hält die Lehrer größtenteils für unfähig.",
+        ],
+      },
+      {
+        // Lösung: B
+        id: "vorwurf_eltern",
+        text: "Die Autorin wirft den Eltern vor, dass sie",
+        answers: [
+          "ihren Kindern bei Problemen nicht helfen.",
+          "nicht mit den Lehrern reden wollen.",
+          "ihre Kinder zu Ungehorsam auffordern.",
+          "keine Ahnung vom Schulalltag haben.",
+        ],
+      },
+      {
+        // Lösung: A
+        id: "internet",
+        text: "Im Hinblick auf das Internet meint die Autorin, dass",
+        answers: [
+          "es für bösartige Angriffe die ideale Plattform ist. ",
+          "ein richtiger Einsatz zu Verbesserungen führen kann.",
+          "man beleidigende Seiten sperren müsste.",
+          "man die dort eingestellten Inhalte genau prüfen muss.",
+        ],
+      },
+    ],
+  },
+  {
     id: "empirie",
     component: <Empirie />,
     bionicComponent: <EmpirieBionic />,
@@ -121,135 +183,6 @@ export const content: Text[] = [
           "bewerteten die Schwimmleistungen der Schüler.",
           "notierten die zurückgelegten Distanzen der Schüler.",
           "organisierten die Sponsorensuche für die Schüler.",
-        ],
-      },
-    ],
-  },
-  {
-    id: "lehrerin",
-    component: <Lehrerin />,
-    bionicComponent: <LehrerinBionic />,
-    questions: [
-      {
-        // Lösung: B
-        id: "gefuehle_beruf",
-        text: "Was empfindet die Autorin, wenn sie ihren Beruf nennt?",
-        answers: [
-          "Sie fühlt sich besonders wichtig.",
-          "Sie hat gemischte Gefühle.",
-          "Sie ist wütend auf die Fragenden.",
-          "Sie empfindet nichts.",
-        ],
-      },
-      {
-        // Lösung: A
-        id: "warum_lehrer",
-        text: "Warum ergriff die Autorin den Lehrerberuf?",
-        answers: [
-          "Sie wollte jungen Menschen etwas fürs Leben mitgeben.",
-          "Sie suchte eine Arbeit, die viel Freiraum bietet.",
-          "Ihr gefiel die Sicherheit des Arbeitsplatzes.",
-          "Sie suchte eine Stelle mit Verantwortung.",
-        ],
-      },
-      {
-        // Lösung: C
-        id: "oeffentlichkeit_meinung",
-        text: "Wie steht nach Meinung der Autorin die Öffentlichkeit zum Beruf des Lehrers?",
-        answers: [
-          "Man gibt den Lehrern Schuld an gesellschaftlichen Missständen.",
-          "Die vormals negative Beurteilung der Lehrer ist zurückgegangen.",
-          "Die Beurteilung schwankt zwischen Anerkennung und harter Kritik.",
-          "Man hält die Lehrer größtenteils für unfähig.",
-        ],
-      },
-      {
-        // Lösung: B
-        id: "vorwurf_eltern",
-        text: "Die Autorin wirft den Eltern vor, dass sie",
-        answers: [
-          "ihren Kindern bei Problemen nicht helfen.",
-          "nicht mit den Lehrern reden wollen.",
-          "ihre Kinder zu Ungehorsam auffordern.",
-          "keine Ahnung vom Schulalltag haben.",
-        ],
-      },
-      {
-        // Lösung: A
-        id: "internet",
-        text: "Im Hinblick auf das Internet meint die Autorin, dass",
-        answers: [
-          "es für bösartige Angriffe die ideale Plattform ist. ",
-          "ein richtiger Einsatz zu Verbesserungen führen kann.",
-          "man beleidigende Seiten sperren müsste.",
-          "man die dort eingestellten Inhalte genau prüfen muss.",
-        ],
-      },
-      {
-        // Lösung: D
-        id: "schwierige_verhaeltnisse",
-        text: "Angesichts der geschilderten schwierigen Verhältnisse plädiert die Autorin dafür, dass",
-        answers: [
-          "die Eltern mehr erzieherische Verantwortung übernehmen.",
-          "die Gesellschaft den Lehrern mehr Unterstützung gewährt.",
-          "die Schüler unter weniger Leistungsdruck gestellt werden.",
-          "die Lehrer über ihren Beitrag zur Veränderung nachdenken.",
-        ],
-      },
-      // ---
-      {
-        // Lösung: A
-        id: "kultur_der_rueckmeldung",
-        text: "Was versteht die Autorin unter einer “Kultur der Rückmeldung”?",
-        answers: [
-          "Einen Austausch zwischen Lehrern, Schülern und Eltern.",
-          "Die Fähigkeit der Lehrer, Selbstkritik zu üben.",
-          "Die Kunst, Kritik konstruktiv zu formulieren.",
-          "Ein positives Feedback vonseiten der Eltern.",
-        ],
-      },
-      {
-        // Lösung: C
-        id: "erwartungen_lehrkraefte",
-        text: "Was sagt die Autorin über die Erwartungen an die Lehrkräfte?",
-        answers: [
-          "Sie sind extremen Schwankungen unterworfen.",
-          "Sie verlieren den Bezug zur realen Schulsituation.",
-          "Sie sind äußerst vielfältig und anspruchsvoll.",
-          "Sie verhindern die Entfaltung der Lehrkräfte.",
-        ],
-      },
-      {
-        // Lösung: B
-        id: "angst_lehrer",
-        text: "Nach Meinung der Autorin haben die Lehrer Angst davor,",
-        answers: [
-          "fachlich nicht auf dem Laufenden zu sein.",
-          "von den Schülern nicht akzeptiert zu werden.",
-          "mit Kollegen über ihre Probleme zu reden.",
-          "eingefahrene Gleise im Unterricht zu verlassen.",
-        ],
-      },
-      {
-        // Lösung: A
-        id: "wunsch_autorin",
-        text: "Die Autorin fände es gut, wenn",
-        answers: [
-          "das Kollegium gemeinsam didaktische Neuerungen erarbeitete.",
-          "die Lehrerschaft offensiv auf ihre Probleme aufmerksam machte.",
-          "sich jeder Lehrer der Bewertung durch seine Kollegen stellte.",
-          "man zusammen mit den Schülern neue Lehrmodelle entwickelte.",
-        ],
-      },
-      {
-        // Lösung: D
-        id: "erste_erfahrungen",
-        text: "Erste Erfahrungen mit Lehrerbewertungen zeigen, dass",
-        answers: [
-          "wenige Schüler bereit sind, konstruktive Kritik zu üben.",
-          "die Schüler schnell das Interesse am Mitmachen verlieren.",
-          "die Schüler die Verletzung ihrer Anonymität fürchten.",
-          "Schülerkritik nur von wenigen Lehrern thematisiert wird.",
         ],
       },
     ],
